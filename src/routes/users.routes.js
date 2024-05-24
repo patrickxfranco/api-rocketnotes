b3d7822042cdb6731ff39aaa5af1f5e0b0423a82
Patrick Franco <patrickxfranco@gmail.com>
2024-05-24 14:14:36 -0300

    Adiciona a alteração do avatar no banco de dados(não funcional)

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,18 +3,17 @@ const multer = require("multer");
 const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/users.controller");
+const UsersAvatarController = require("../controllers/userAvatar.controller");
 const ensureAuthenticated = require("../middleware/ensureAuthenticated")
 
 const userRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const userController = new UsersController();
+const usersAvatarController = new UsersAvatarController();
 
 userRoutes.post("/", userController.create);
 userRoutes.put("/", ensureAuthenticated, userController.update);
-userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
-  console.log(request.file.filename);
-  response.json();
-});
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), usersAvatarController);
 
 module.exports = userRoutes;
